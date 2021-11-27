694b4ed7eefba1975c1f0e154623b007695290ae
Agungfir08 <agung.firmansyah@ui.ac.id>
2021-11-27 13:00:34 +0700

    CS modul 9 modified

--- app/src/main/java/com/AgungJmartAK/jmart_android/RegisterActivity.java
+++ app/src/main/java/com/AgungJmartAK/jmart_android/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,40 +29,42 @@ public class RegisterActivity extends AppCompatActivity {
         EditText textemail = findViewById(R.id.email_register_layout);
         EditText textpassword = findViewById(R.id.password_register_layout);
         Button buttonregister = findViewById(R.id.login_button);
-
-        Response.Listener<String> listener = new Response.Listener<String>() {
+        buttonregister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(String response) {
-                try{
-                    JSONObject object = new JSONObject(response);
-                    if(object != null){
-                        Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_LONG);
-                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                        startActivity(intent);
+            public void onClick(View v) {
+                Response.Listener<String> listener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject object = new JSONObject(response);
+                            if(object != null){
+                                Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_LONG);
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this, "Register Error!", Toast.LENGTH_LONG);
+                        }
                     }
-                }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                    Toast.makeText(RegisterActivity.this, "Register Error!", Toast.LENGTH_LONG);
-                }
+                };
+
+                Response.ErrorListener errorListener = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, "Login Error!", Toast.LENGTH_LONG);
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(textname.getText().toString(),
+                        textemail.getText().toString(),
+                        textpassword.getText().toString(),
+                        listener,
+                        null);
+                RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                requestQueue.add(registerRequest);
             }
-        };
-
-        Response.ErrorListener errorListener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(RegisterActivity.this, "Login Error!", Toast.LENGTH_LONG);
-            }
-        };
-
-        RegisterRequest registerRequest = new RegisterRequest(textname.getText().toString(),
-                textemail.getText().toString(),
-                textpassword.getText().toString(),
-                listener,
-                null);
-        RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
-        requestQueue.add(registerRequest);
-
-
+        });
     }
 }
